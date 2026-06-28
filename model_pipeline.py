e86448a298debf796055978df6a0d72543a84c42
manelbnalii <manel.benali@esprit.tn>
2026-06-28 20:55:22 +0100

    fix missing imports classification_report confusion_matrix

--- model_pipeline.py
+++ model_pipeline.py
@@ -6,7 +6,7 @@ import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, classification_report, confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
 import matplotlib.pyplot as plt
 
